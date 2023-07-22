159a9df70c4dd60fd86d30b76dfc9e82e0a78f53
Hafiz <hafizmluqman4789@gmail.com>
2023-07-22 15:23:37 +0500

    Sripping

---
+++ Stripping Names/index.js
@@ -0,0 +1,6 @@
+//Stripping the string using trim() method
+//creating the string
+var trip = "\n\t    Muhammad Ali Raza     \t\n";
+//Print Name Before and After Strip
+console.log("Before stripping:'" + trip + "'");
+console.log("After stripping :'" + trip.trim() + "'");
---
+++ Stripping Names/index.ts
@@ -0,0 +1,9 @@
+
+//Stripping the string using trim() method
+
+//creating the string
+let trip:string = "\n\t    Muhammad Ali Raza     \t\n";
+
+//Print Name Before and After Strip
+console.log("Before stripping:'" + trip + "'");
+console.log("After stripping :'" + trip.trim() + "'");
