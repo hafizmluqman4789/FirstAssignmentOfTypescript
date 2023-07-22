6b2c3df7c1c89c7b37923235d5bff40bf520358d
Hafiz <hafizmluqman4789@gmail.com>
2023-07-22 09:12:26 +0500

    Lower

---
+++ LowerCasetoUpperAndViceVersa/index.js
@@ -0,0 +1,10 @@
+/*This Line of Code convert the text in the lower case
+to upper case in typescrip*/
+//Declare the string
+var firstName;
+//Initialze the string
+firstName = "Muhammad Bin";
+//Print the Name before converting into UpperCase
+console.log("First Namr Before Converting into the UpperCase\n", firstName);
+//Print thhe Name After Converting into the UpperCase 
+console.log("First Name After Converting into the UpperCase\n", firstName.toUpperCase());
---
+++ LowerCasetoUpperAndViceVersa/index.ts
@@ -0,0 +1,15 @@
+/*This Line of Code convert the text in the lower case 
+to upper case in typescrip*/
+
+//Declare the string
+let firstName:string;
+
+//Initialze the string
+firstName="Muhammad Bin";
+
+//Print the Name before converting into UpperCase
+console.log("First Namr Before Converting into the UpperCase\n",firstName);
+
+//Print thhe Name After Converting into the UpperCase 
+console.log("First Name After Converting into the UpperCase\n",firstName.toUpperCase());
+
