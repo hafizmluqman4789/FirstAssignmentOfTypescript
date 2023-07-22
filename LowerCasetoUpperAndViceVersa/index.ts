2d010b92f75e9029892fec2edf6485c6788818fb
Hafiz <hafizmluqman4789@gmail.com>
2023-07-22 11:09:19 +0500

    Upper

--- LowerCasetoUpperAndViceVersa/index.ts
+++ LowerCasetoUpperAndViceVersa/index.ts
@@ -5,13 +5,23 @@ to upper case in typescrip*/
 
 //Declare the string
 let firstName:string;
+let lastName:string;
 
 //Initialze the string
 firstName="Muhammad Bin";
+lastName="SALMAN Al SAUD"
 
 //Print the Name before converting into UpperCase
-console.log("First Namr Before Converting into the UpperCase\n",firstName);
+console.log("\nFirst Namr Before Converting into the UpperCase:",firstName,"\n");
 
 //Print thhe Name After Converting into the UpperCase 
-console.log("First Name After Converting into the UpperCase\n",firstName.toUpperCase());
+console.log("First Name After Converting into the UpperCase:",firstName.toUpperCase(),"\n");
 
+//Conversion of text from upper case to lower Case
+console.log("\n\n\t\tConversion of Text From Upper Case To Lower Case!!\n\n");
+
+//Print the Name before converting into LowerCase
+console.log("\nLast Namr Before Converting into the LowerCase:",lastName,"\n");
+
+//Print the Name After Converting into the LowerCase 
+console.log("Last Name After Converting into the LowerCase:",lastName.toLowerCase(),"\n");
