6f1e46d7f5220589b7f2141186783a8f33074793
Hafiz <hafizmluqman4789@gmail.com>
2023-08-10 20:26:26 +0500

    Number8

---
+++ Number Eight/index.ts
@@ -0,0 +1,21 @@
+// Perform Basic Arithmatic Operations e.g. Addition, Subtraction, Multiplication and Division.
+
+//Addition
+
+let Add=4+4;
+console.log('Addition : '+Add);
+
+//Subtraction
+
+let Subtract=16-8;
+console.log('Subtraction : '+Subtract);
+
+//Multiplication
+
+let Multiply=4*2;
+console.log('Multiplication : '+Multiply);
+
+//Division
+
+let Divide=16/2;
+console.log('Division : '+Divide);
