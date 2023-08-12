20cb51cc7725b8b6c65aa1773596211449ab203b
Hafiz <hafizmluqman4789@gmail.com>
2023-08-12 16:14:46 +0500

    Four Lines

---
+++ Create Four Lines/index.ts
@@ -0,0 +1,15 @@
+//Creating Four lines and Print 8 on each line
+
+console.log("_".repeat(70));
+console.log("\t\t\t",4+4);
+
+console.log("_".repeat(70));
+console.log("\t\t\t",4*2);
+
+console.log("_".repeat(70));
+console.log("\t\t\t",16-8);
+
+console.log("_".repeat(70));
+console.log("\t\t\t",16/2);
+
+console.log("_".repeat(70));
