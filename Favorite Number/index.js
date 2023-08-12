debcf1d03d40277141347741c4022a60b0d60372
Hafiz <hafizmluqman4789@gmail.com>
2023-08-12 17:05:05 +0500

    Favorite Number

---
+++ Favorite Number/index.js
@@ -0,0 +1,6 @@
+"use strict";
+//Store te favorite number invariable then reveal through the message
+let myFavoriteNumber;
+myFavoriteNumber = 32;
+let message = 'My Favorite Number is: ';
+console.log(message + myFavoriteNumber);
---
+++ Favorite Number/index.ts
@@ -0,0 +1,9 @@
+//Store te favorite number invariable then reveal through the message
+
+let myFavoriteNumber;
+
+myFavoriteNumber=32;
+
+let message:string='My Favorite Number is: ';
+
+console.log(message+myFavoriteNumber);
